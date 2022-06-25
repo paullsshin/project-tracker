64aea063af82697e837f05cf4f973b0bb9200eb1
Niklas Solomon <niklassolomon@gmail.com>
2022-06-25 13:31:35 -0700

    began creating variables for project info

---
+++ assets/JS/script.js
@@ -0,0 +1,11 @@
+var projectFormEl = $("#project-form");
+var instructionsEl = $('<h2>'); 
+var addProject = $("button");
+
+var projectInfo = [
+    'Project Name',
+    'Project Type',
+    'Due Date',
+    'Hourly Rate',
+];
+
